995eba13515d826153f26967cac149cd64e07b2e
cloud-rohit-1997 <160157203+cloud-rohit-1997@users.noreply.github.com>
2024-08-14 12:08:12 +0530

    updated the code

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "ami" {
   description = "The ID of the Amazon Machine Image (AMI) to use for the instance"
   type        = string
-  default     = "ami-0ad21ae1d0696ad58" # Default value, can be overridden
+  default     = "ami-04a81a99f5ec58529" # Default value, can be overridden
 }
 
 variable "instance_type" {
