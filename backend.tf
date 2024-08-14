dfad1a595fad8a8d2e21bdc53a91f7077d0bae5c
cloud-rohit-1997 <160157203+cloud-rohit-1997@users.noreply.github.com>
2024-08-14 11:48:01 +0530

    Updated backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "my-dev-terra-jen"
+    bucket = "my-dev-terra-jen-01"
     key    = "terraform/tf.state"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
